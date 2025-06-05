73ed814feb80533bc1872a35980920bcb1a81923
unknown <laveshtopani18@gmail.com>
2025-06-05 18:07:04 +0530

    feat/interview

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -6,15 +6,15 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const gen_ai = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = gen_ai.getGenerativeModel({
-    model:"gemini-1.5-flash",
-})
+  model: "gemini-1.5-flash",
+});
 
-export const generateAiInsights = async(industry)=>{
-    const prompt = `
+export const generateAiInsights = async (industry) => {
+  const prompt = `
           Analyze the current state of the ${industry} industry and provide insights in ONLY the following JSON format without any additional notes or explanations:
           {
             "salaryRanges": [
-              { "role": "string", "min": number, "max": number, "median": number, "location": "string" }
+              { "role": "string", "min": number, "max": number, "median": number, "location": "string",minRupees:number,maxRupees:number,medianRupees:number }
             ],
             "growthRate": number,
             "demandLevel": "HIGH" | "MEDIUM" | "LOW",
@@ -28,45 +28,45 @@ export const generateAiInsights = async(industry)=>{
           Include at least 5 common roles for salary ranges.
           Growth rate should be a percentage.
           Include at least 5 skills and trends.
+          IN min,max,median i want US salaries and in minRupees,maxRupees,medianRupees i want salary based on india that is convert dollars to rupees
         `;
 
-        const result = await model.generateContent(prompt);
-        const response = result.response.text().replace(/```(?:json)?\n?/g,"").trim();
+  const result = await model.generateContent(prompt);
+  const response = result.response
+    .text()
+    .replace(/```(?:json)?\n?/g, "")
+    .trim();
 
-        return JSON.parse(response);
+  return JSON.parse(response);
+};
 
-
-
-}
-
-export async function getIndustryInsights(){
-     const { userId } = await auth();
+export async function getIndustryInsights() {
+  const { userId } = await auth();
   if (!userId) throw new Error("Unaothorized");
 
   const user = await db.user.findUnique({
     where: {
       clerkUserId: userId,
     },
-    include:{
-        industryInsight:true
-    }
+    include: {
+      industryInsight: true,
+    },
   });
 
   if (!user) throw new Error("User not found");
 
-  if(!user.industryInsight)
-  {
+  if (!user.industryInsight) {
     const insights = await generateAiInsights(user.industry);
 
     const industryInsights = await db.industryInsights.create({
-         data:{
-                industry:user.industry,
-                ...insights,
-                nextUpdate: new Date(Date.now() + 7*24*60*60*1000)
-            }
-    })
+      data: {
+        industry: user?.industry,
+        ...insights,
+        nextUpdate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      },
+    });
 
-    return industryInsights
+    return industryInsights;
   }
   return user.industryInsight;
 }
